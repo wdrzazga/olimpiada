f1b31d9b79d8f3896e816767d34e9209eaf4fa01
wdrzazga <popownica@gmail.com>
2025-10-07 17:54:19 +0200

    finished the kab task

--- olimiadaXX/kab.py
+++ olimiadaXX/kab.py
@@ -1,40 +1,34 @@
-import math
+import functools
 from datetime import datetime
 
-N = 6
+N = int(input("Podaj ilosc kabelków)"))
+sockets = []
 
-coord = [(0, 1), (10, 0), (0, 0), (20, 1), (1, 11), (1, 2)]
-coord2 = 33333 * coord
-print(len(coord2))
+for i in range(2*N):
+    print("Podaj wspólrzedne gniazdka (oddzielone spacja): " + str(i))
+    s1 = input(": ")
+
+    nums = s1.split()
+    point1 = int(nums[0]), int(nums[1])
+    sockets.append(point1)
+
 start = datetime.now()
 
 
-def find_nearest_socket(point):
-    coord.remove(point)
-    distances = []
-    for socket in coord2:
-        a = abs(point[0] - socket[0])
-        b = abs(point[1] - socket[1])
-        distances.append(a ** 2 + b ** 2)
-
-    return distances.index(min(distances)), min(distances)
-
-
-def orient(P, Q, R):
-    return (Q[0] - P[0]) * (R[1] - P[1]) - (Q[1] - P[1]) * (R[0] - P[0])
-
-
-def intersect(s1, s2):
-    A = s1[0]
-    B = s1[1]
-    C = s2[0]
-    D = s2[1]
-
-    return (orient(C, D, A) * orient(C, D, B) < 0) and (orient(A, B, C) * orient(A, B, D) < 0)
+def comparator(p1, p2):
+    if p1[0] < p2[0]:
+        return -1
+    elif p1[0] > p2[0]:
+        return 1
+    else:
+        if p1[1] < p2[1]:
+            return -1
+        elif p1[1] > p2[1]:
+            return 1
+        else:
+            return 0
 
 
-socket = (20, 1)
-nearest_socket = find_nearest_socket(socket)
-
-end = datetime.now()
-print(end - start)
+sorted_points = sorted(sockets, key=functools.cmp_to_key(comparator))
+for i in range(0, len(sorted_points), 2):
+    print(sorted_points[i], sorted_points[i+1])
